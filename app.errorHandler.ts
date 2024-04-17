4c8da51fd55c7f1b79a9cfc0d05be2f3c9b4c8be
fshchudlo <fshchudlo@gmail.com>
2024-04-17 13:11:33 +0300

    Nifty cleanup

--- app.errorHandler.ts
+++ app.errorHandler.ts
@@ -5,11 +5,7 @@ import { SlackAPIAdapterCachedDecorator } from "./gateways/SlackAPIAdapterCached
 import express, { NextFunction } from "express";
 
 export default function configureErrorHandler(expressReceiver: ExpressReceiver, slackAPI: SlackAPIAdapterCachedDecorator) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     expressReceiver.router.use(async (error: any, req: express.Request, res: express.Response, next: NextFunction) => {
-        if (res.headersSent) {
-            return next(error)
-        }
         const errorMessage = ["Error processing webhook.", `Error: ${util.inspect(error, false, 8)}.`, `Payload: ${util.inspect(req.body, false, 8)}`].join("\n\n");
         console.error(errorMessage);
         try {
@@ -22,6 +18,10 @@ export default function configureErrorHandler(expressReceiver: ExpressReceiver, 
         } catch (error) {
             console.error("Error during sending message to the diagnostic channel", error);
         }
-        res.sendStatus(500);
+        if (res.headersSent) {
+            return next(error);
+        } else {
+            res.sendStatus(500);
+        }
     });
 }
