ad5c512b3260f852eb237db1c7b9ba7323c5a663
fshchudlo <fshchudlo@gmail.com>
2024-05-01 14:51:04 +0300

    Nifty fixes

--- webhook-handler/use-cases/helpers/getPullRequestCompletionAction.ts
+++ webhook-handler/use-cases/helpers/getPullRequestCompletionAction.ts
@@ -9,19 +9,19 @@ export function getPullRequestCompletionAction(payload: PullRequestBasicNotifica
     switch (payload.eventKey) {
         case "pr:deleted":
             return {
-                text: `The pull request was deleted by ${formatUserName(payload.actor)}.`,
+                text: `Pull request was deleted by ${formatUserName(payload.actor)}.`,
                 emoji: ":no_entry_sign:",
                 reaction: "no_entry_sign"
             };
         case "pr:declined":
             return {
-                text: `The pull request was declined by ${formatUserName(payload.actor)}.`,
+                text: `Pull request was declined by ${formatUserName(payload.actor)}.`,
                 emoji: ":no_entry_sign:",
                 reaction: "no_entry_sign"
             };
         case "pr:merged":
             return {
-                text: `The pull request was merged by ${formatUserName(payload.actor)}. Well done, thank you all.`,
+                text: `Pull request was merged by ${formatUserName(payload.actor)}. Well done, thank you all.`,
                 emoji: ":white_check_mark:",
                 reaction: "white_check_mark"
             };
