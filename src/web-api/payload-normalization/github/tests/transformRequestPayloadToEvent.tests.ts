69908f6e061a329e33c3e2aa449ca5c04fc5e07e
Fedor Shchudlo <fedor.shchudlo@webpros.com>
2024-11-01 11:08:08 +0200

    Added `X-GitHub-Event` header handling to distinct event types

--- src/web-api/payload-normalization/github/tests/transformRequestPayloadToEvent.tests.ts
+++ src/web-api/payload-normalization/github/tests/transformRequestPayloadToEvent.tests.ts
@@ -1,11 +1,22 @@
 import { transformRequestPayloadToEvent } from "../transformRequestPayloadToEvent";
 
 describe("transformRequestPayloadToEvent.tests", () => {
+    it("Should throw Error on unknown event type", async () => {
+        expect.assertions(1);
+
+        try {
+            await transformRequestPayloadToEvent("unknown event type" as any, {
+                action: "unknown action"
+            } as any, null, null);
+        } catch (error) {
+            expect((error as Error).message).toBe("\"unknown event type\" event type is unknown.");
+        }
+    });
     it("Should throw Error on unknown action type", async () => {
         expect.assertions(1);
 
         try {
-            await transformRequestPayloadToEvent({
+            await transformRequestPayloadToEvent("pull_request", {
                 action: "unknown action"
             } as any, null, null);
         } catch (error) {
