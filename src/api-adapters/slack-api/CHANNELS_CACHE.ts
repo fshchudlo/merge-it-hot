b07117eb82c7611dba799980893dcb08999d7c66
fshchudlo <fshchudlo@gmail.com>
2024-12-05 11:46:39 +0200

    Inverted cross-package dependencies

--- src/api-adapters/slack-api/CHANNELS_CACHE.ts
+++ src/api-adapters/slack-api/CHANNELS_CACHE.ts
@@ -1,5 +1,5 @@
-import { SlackChannelInfo } from "./SlackChannelProvisioner";
 import { CacheMetricsWrapper } from "../cache-metrics-wrapper/CacheMetricsWrapper";
+import { SlackChannelInfo } from "../../pr-events-handler/slack-api-ports";
 
 export const CHANNELS_CACHE = new CacheMetricsWrapper<SlackChannelInfo>(
     "channels",
